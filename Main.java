19f0c1fdf79d4fb23894066cfe51ddf807337d8f
Stevennnncz <steven070295@gmail.com>
2022-09-24 13:40:33 -0600

    a

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ import java.util.Date;
  */
 public class Main {
     public static void main(String[] args) {
-        Date fecha = new Date(2003,02,7);
 
+        Date fecha = new Date(2003,02,7);
         Estudiantes estudiantes = new Estudiantes("E1", 1, fecha , 15, 1, "San Carlos");
 
 
