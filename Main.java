0f49e7a355795dc67e2ba2e2b29ddce444f1a0a3
Stevennnncz <steven070295@gmail.com>
2022-09-24 13:40:20 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import java.util.Date;
+
 /**
  * @author Brithanny Arguello, Steven Chacón y Jorge Gonzales
  */
 public class Main {
     public static void main(String[] args) {
-        
+        Date fecha = new Date(2003,02,7);
+
+        Estudiantes estudiantes = new Estudiantes("E1", 1, fecha , 15, 1, "San Carlos");
+
+
+
     }
 }
