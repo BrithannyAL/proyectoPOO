7e0e52a5383929f74a5a7b2f1792cf112be74ce5
BrithannyAL <brijesus.12@gmail.com>
2022-09-24 12:15:01 -0600

    metodos agregar usuarios

--- Clases/Usuarios/Coordinador.java
+++ Clases/Usuarios/Coordinador.java
@@ -24,6 +24,13 @@ public class Coordinador extends Usuarios {
         super(nom, tel, cor, usu, con);
     }
     /**
+     * Nos devuelve la lista de usuarios de tipo coordinador registrados.
+     * @return coordinadores (ArrayList<Coordinador>)
+     */
+    public ArrayList<Coordinador> getCoordinadores() {
+        return coordinadores;
+    }
+    /**
      * Método que permite agregar un nuevo cordinador a la lista de usuarios coordinadores de la clase.
      * @param nom (String)
      * @param tel (String[])
@@ -31,8 +38,21 @@ public class Coordinador extends Usuarios {
      * @param usu (String)
      * @param con (String)
      */
-    public void crearUsuarioCoordinador(String nom, String[] tel, String cor, String usu, String con){
+    public void crearUsuarioCoordinador(String nom, String[] tel, String cor, String usu, String con) {
         Coordinador nuevo = new Coordinador(nom, tel, cor, usu, con);
         this.coordinadores.add(nuevo);
+    }
+    /**
+     * Método que permite agregar un nuevo usuario de tipo profesor a la lista de la clase profesor.
+     * Solo el usuario de tipo coordinador puede agregar un profesor a la lista.
+     * @param nom (String)
+     * @param tel (String[])
+     * @param cor (String)
+     * @param usu (String)
+     * @param con (String)
+     */
+    public void crearUsuarioProfesor(String nom, String[] tel, String cor, String usu, String con) {
+        Profesor nuevoProfesor = new Profesor(nom, tel, cor, usu, con);
+        nuevoProfesor.setProfesores(nuevoProfesor);
     }
 }
--- Clases/Usuarios/Profesor.java
+++ Clases/Usuarios/Profesor.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+
 /**
  * Clase que describe a los usuarios de tipo Profesor
  * Clase heredada de la clase Usuarios
  * @author Brithanny Arguello, Steven Chacón y Jorge Gonzáles
  */
 public class Profesor extends Usuarios {
+    /**
+     * Atributos
+     */
+    private ArrayList<Profesor> profesores;
     /**
      * Constructor de la clase que se hereda de Usuarios
      * @param nom (String)
@@ -14,5 +20,19 @@ public class Profesor extends Usuarios {
      */
     public Profesor(String nom, String[] tel, String cor, String usu, String con) {
         super(nom, tel, cor, usu, con);
+    }
+    /**
+     * Deuelve la lista de los usuarios de tipo profesor registrados
+     * @return profesores (ArrayList<Profesor>)
+     */
+    public ArrayList<Profesor> getProfesores() {
+        return profesores;
+    }
+    /**
+     * Permite agregar un nuevo profesor a la lista de profesores registrados
+     * @param prof (Profesor)
+     */
+    public void setProfesores(Profesor prof) {
+        this.profesores.add(prof);
     }
 }
