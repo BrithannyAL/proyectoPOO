4ec6341b0f2a732197b9027c14da003101238453
Stevennnncz <steven070295@gmail.com>
2022-09-25 14:48:59 -0600

    creacion de mas usuarios

--- Main.java
+++ Main.java
@@ -6,13 +6,17 @@ public class Main {
     public static void main(String[] args) {
         /* CARGAR DATOS DE ESTUDIANTES */
         Estudiantes [] listaEstudiantes = new Estudiantes[10];
-        listaEstudiantes[0] = new Estudiantes(
-            "Veronica",
-            "2022888888",
-            new Date(104, 4, 12),
-            (short) 19,
-            true,
-            "Santa Rosa de Pocosol");
+        listaEstudiantes[0] = new Estudiantes("Veronica","1",new Date(2003, 4, 12),
+            (short) 19,true,"Santa Rosa de Pocosol");
+        listaEstudiantes[1] = new Estudiantes("Juan","2",new Date(2003, 6, 12),
+            (short) 19,false,"Santa Clara");
+        listaEstudiantes[2] = new Estudiantes("Pedro","3",new Date(2003, 3, 5),
+            (short) 19,false,"Fortuna");
+        listaEstudiantes[3] = new Estudiantes("Maria","4",new Date(2003, 2, 6),
+            (short) 19,true,"Florencia");
+        listaEstudiantes[4] = new Estudiantes("Angel","5",new Date(2003, 7, 23),
+            (short) 19,true,"CQ");
+
         
     }
 }
