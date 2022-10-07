d4541b2a116a2170da5bf4ab47f80f6568762131
Stevennnncz <steven070295@gmail.com>
2022-10-07 11:07:04 -0600

    return

---
+++ Usuarios/Coordinador.java
@@ -0,0 +1,23 @@
+package Usuarios;
+
+/**
+ * Clase que describe el usuario de tipo coordinador y sus respectivas funciones
+ * (comportamientos)
+ * Clase que hereda la clase Usuarios
+ * 
+ * @author Brithanny Arguello, Steven Chacón y Jorge Gonzáles
+ */
+public class Coordinador extends Usuarios {
+    /**
+     * Constructor de la clase que se hereda de Usuarios
+     * 
+     * @param nom (String) nombre completo
+     * @param tel (String[]) lista de telefonos
+     * @param cor (String) correo
+     * @param usu (String) usuario
+     * @param con (String) contraseña
+     */
+    public Coordinador(String nom, String[] tel, String cor, String usu, String con) {
+        super(nom, tel, cor, usu, con);
+    }
+}
